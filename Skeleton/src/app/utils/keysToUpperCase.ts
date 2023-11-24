612363a25ec2bf999a7352fb8c5a446335c173ef
Vitoko75 <138317928+Vitoko75@users.noreply.github.com>
2023-11-24 13:56:28 -0300

    Delete Skeleton directory

--- Skeleton/src/app/utils/keysToUpperCase.ts
+++
@@ -1,6 +0,0 @@
-export const keysToUpperCase = (obj: any) => {
-  return Object.keys(obj).reduce((newObj: any, key: any) => {
-    newObj[key.toUpperCase()] = obj[key];
-    return newObj;
-  }, {});
-};
